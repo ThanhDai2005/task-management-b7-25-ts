79e445c550e8d440381920097c3002fd58243b82
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-10-28 20:40:45 +0700

    9. Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+dotenv.config();
+
 import { connect } from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
-import bodyParser from "body-parser";
-
-dotenv.config();
 
 connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
